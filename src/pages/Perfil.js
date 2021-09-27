6356d7b3e4a67bb843006a736b6ae340d3412732
Natalia Camarinha <natcamarinha@gmail.com>
2021-09-27 18:19:53 -0300

    profile page completa

--- src/pages/Perfil.js
+++ src/pages/Perfil.js
@@ -1,47 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import SearchHeader from '../components/Header/SearchHeader';
 import Footer from '../components/Footer/Footer';
 
-class Perfil extends React.Component {
-  render() {
-    const { email } = this.props;
-    const pageTitle = 'Perfil';
-    return (
-      <div>
-        <SearchHeader value={ pageTitle } />
-        <div>
-          <p data-testid="profile-email">{email}</p>
+export default function Perfil({ history }) {
+  const [email, setEmail] = useState('');
 
-          <button
-            type="button"
-            data-testid="profile-done-btn"
-          >
-            Receitas Feitas
-          </button>
+  useEffect(() => {
+    setEmail(JSON.parse(localStorage.getItem('user')).email);
+  }, []);
 
-          <button
-            type="button"
-            data-testid="profile-favorite-btn"
-          >
-            Receitas Favoritas
-          </button>
-
-          <button
-            type="button"
-            data-testid="profile-logout-btn"
-          >
-            Sair
-          </button>
-        </div>
-        <Footer />
-      </div>
-    );
+  function handleClick() {
+    localStorage.clear();
+    history.push('/');
   }
+
+  const pageTitle = 'Perfil';
+  return (
+    <div>
+      <SearchHeader value={ pageTitle } />
+      <div>
+        <p data-testid="profile-email">{ email }</p>
+      </div>
+
+      <div>
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+          onClick={ () => history.push('/receitas-feitas') }
+        >
+          Receitas Feitas
+        </button>
+
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+          onClick={ () => history.push('/receitas-favoritas') }
+        >
+          Receitas Favoritas
+        </button>
+
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+          onClick={ handleClick }
+        >
+          Sair
+        </button>
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 Perfil.propTypes = {
-  email: PropTypes.string.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
 };
-
-export default Perfil;
