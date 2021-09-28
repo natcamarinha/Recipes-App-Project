f7ab3de8c899ffbf2d1b2f7d6f76a9f4fea834d6
Alexander <alexanderteodorocastro95@hotmail.com>
2021-09-28 15:33:55 -0300

    test4

--- src/pages/ComidasProgresso.js
+++ src/pages/ComidasProgresso.js
@@ -12,9 +12,10 @@ export default class ComidasProgresso extends React.Component {
     this.state = {
       loading: true,
       id,
+      Disabled: 'disabled',
     };
 
-    this.handleClick = this.handleClick.bind(this);
+    this.checkAll = this.checkAll.bind(this);
     this.fetchAPI = this.fetchAPI.bind(this);
     this.mealDetails = this.mealDetails.bind(this);
   }
@@ -23,11 +24,34 @@ export default class ComidasProgresso extends React.Component {
     this.fetchAPI();
   }
 
-  handleClick(e) {
-    const checkInput = e.target.checked;
-    const linha1 = checkInput.parentElement('div');
-    if (checkInput.value === true) {
-      linha1.classList.add('linha2');
+  handleClick({ target }) {
+    if (target.checked === true) {
+      target.parentElement('label').className.add('linha2');
+    } else {
+      target.parentElement('label').className.add('');
+      target.checked = false;
+    }
+  }
+
+  checkAll() {
+    const inputs = document.querySelectorAll('input');
+    let count = 0;
+    inputs.forEach((input) => {
+      if (input.checked === true) {
+        count += 1;
+      }
+    });
+    const { meals } = this.state;
+    const meal = meals.meals[0];
+    const ingredientArray = Object
+      .entries(meal)
+      .filter((ingredient) => (ingredient[0].includes('Ingredient')
+      && ingredient[1] !== '' && ingredient[1] !== null));
+    console.log(ingredientArray.length, count);
+    if (count === ingredientArray.length) {
+      this.setState({
+        Disabled: '',
+      });
     }
   }
 
@@ -36,8 +60,6 @@ export default class ComidasProgresso extends React.Component {
     const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
     const response = await fetch(url);
     const recipe = await response.json();
-    console.log(recipe);
-    console.log(Object.keys(recipe.meals[0]));
     this.setState({
       meals: recipe,
       loading: false,
@@ -59,23 +81,21 @@ export default class ComidasProgresso extends React.Component {
       .push(Object
         .entries(meal)
         .filter((ingredient) => (ingredient[0].includes('Measure'))));
-    console.log(ingredientArray);
-    console.log(measureArray);
 
     for (let index = 0; index < ingredientArray[0].length; index += 1) {
       if (ingredientArray[0][index][1]) {
         ingredientsWithMeasures.push(
-          <div
-            htmlFor="checkbox"
+          <label
+            htmlFor="ingredient"
+            className="linha1"
             data-testid={ `${index}-ingredient-step` }
             key={ index }
           >
             <input
-              className="checkbox1"
-              onClick={ this.handleClick }
               type="checkbox"
-              id="checkbox"
-              name="checkbox"
+              name="ingredient"
+              className="checked"
+              onClick={ this.checkAll }
               key={ ingredientArray[0][index][1] }
               data-testid={ `${index}-ingredient-name-and-measure` }
             />
@@ -84,7 +104,7 @@ export default class ComidasProgresso extends React.Component {
             -
             {' '}
             {measureArray[0][index][1]}
-          </div>,
+          </label>,
         );
       }
     }
@@ -118,30 +138,31 @@ export default class ComidasProgresso extends React.Component {
         <div data-testid="instructions">
           {meal.strInstructions}
         </div>
-        <div>
-          <Link to="/receitas-feitas">
-            <button
-              data-testid="finish-recipe-btn"
-              className="botaoFinalizar"
-              type="button"
-              Disabled="disabled"
-            >
-              Finalizar receita
-            </button>
-          </Link>
-        </div>
       </div>
 
     );
   }
 
   render() {
-    const { loading } = this.state;
+    const { loading, Disabled } = this.state;
+    console.log(Disabled);
 
     return (
       <div>
         {loading ? <Loading />
           : this.mealDetails()}
+        <div>
+          <Link to="/receitas-feitas">
+            <button
+              data-testid="finish-recipe-btn"
+              className="botaoFinalizar"
+              type="button"
+              disabled={ Disabled }
+            >
+              Finalizar receita
+            </button>
+          </Link>
+        </div>
       </div>
     );
   }
